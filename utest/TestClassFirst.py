5f9fe1fba236316caec4e59978c6fb2eeaae6eff
EvaSokol <EvaSokol@gmail.com>
2019-01-19 09:08:43 +0200

    Test class updated to use class variables

--- utest/TestClassFirst.py
+++ utest/TestClassFirst.py
@@ -9,20 +9,24 @@ def suite():
 
 
 class TestClassFirst(BaseTest):
+    test_variable_one = ''
+    test_variable_two = ''
 
-    # @classmethod
-    # def setUpClass(cls):
-    #     # print cls.method_return_string('String returned from Test setUpClass')
-    #     # print cls.method_echo_string('String echo from Test setUpClass')
-    #     print 'End of Test setUpClass'
+    @classmethod
+    def setUpClass(cls):
+        cls.test_variable_one = cls.method_return_string('String returned from Test setUpClass')
+        cls.test_variable_two = cls.method_echo_string('String echo from Test setUpClass')
+        print 'End of Test setUpClass'
 
     def test_01_first(self):
+        print self.test_variable_one
         str = 'first_string'
         result = self.method_return_string(str)
         print result
         self.assertEqual(str, result, 'No string %s in result' % str)
 
     def test_02_second(self):
+        print self.test_variable_two
         str = 'second_string'
         result = self.method_echo_string(str)
         print result
